37803257372d6063f8e3d179545129004c4fc2d5
Lee-Rhaan <rhaanking@gmail.com>
2022-11-01 13:26:31 +0200

    updated port scanner batch script with placeholder information

--- PortScanner-Script.bat
+++ PortScanner-Script.bat
@@ -4,4 +4,4 @@ echo Enter Host Address below (ip address):
 echo.
 set /p host=
 
-start cmd.exe /k "cd C:\Users\rhaan\Documents\My Projects\PortScanner\target & java -jar PortScanner.jar %host%"
+start cmd.exe /k "cd C:\Users\{jar_location_placeholder} & java -jar PortScanner.jar %host%"
